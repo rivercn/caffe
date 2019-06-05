5bb47ac2128f0639dff0b4e5f6706353d55ece45
rivercn <zhusongsjz@126.com>
2019-06-05 18:24:22 +0800

    modify

--- scripts/create_annoset.py
+++ scripts/create_annoset.py
@@ -3,7 +3,8 @@ import os
 import shutil
 import subprocess
 import sys
-
+caffe_root = '/home/positec/Git/caffe/'
+sys.path.insert(0, caffe_root + 'python')  
 from caffe.proto import caffe_pb2
 from google.protobuf import text_format
 
